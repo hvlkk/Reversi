1fdb0cf83c9598226abc4bd1f1be7f353cb55123
George Halkidis <georgeh98@yahoo.gr>
2023-11-16 16:11:52 +0200

    add methods for printing a move in a formatted way (LetterNumber), returning a Move instance from a formatted input given by the user, and checking if a Move is the default move (row==-1 && col ==-1)

--- src/Move.java
+++ src/Move.java
@@ -73,13 +73,55 @@ public class Move
     }
 
     /**
-     * Used to return the square of the board that a Move corresponds to in a format that is easily readable by the user (e.g. D4, E5)
+     * Used to return the square of the board that a Move corresponds to in a format that is easily readable by the user (e.g. D4 or E5)
      */
     public String formattedIndex() {
-
         // converting the row to a letter and the col to 1-based index
         char rowChar = (char) ('A' + row);
-        return String.valueOf(rowChar) +
-                (col + 1);
+        return String.valueOf(rowChar) + (col + 1);
+    }
+
+    /**
+     * Used to convert the input from the user to a valid Move instance.
+     * @param input The string that the user has entered (e.g. D4 or E5)
+     * @return The move that the user entered.
+     */
+    public static Move readFormattedMove(String input) {
+        input = input.strip();
+        // precautionary check, if the input is invalid we return the default Move
+        if (input.length() != 2 || !isValidRowChar(input.charAt(0)) || !isValidColIndex(input.charAt(1))) {
+            return new Move();
+        }
+        char rowChar = Character.toUpperCase(input.charAt(0));
+        int row = rowChar - 'A';
+        int col = Character.getNumericValue(input.charAt(1)) - 1;
+
+        return new Move(row, col);
+    }
+
+    /**
+     * Ensures the character used to represent the row is valid and not outside the bounds. Accepts both uppercase and lowercase characters.
+     * @param c The character that represents the row.
+     * @return True if the character is a valid row index.
+     */
+    private static boolean isValidRowChar(char c) {
+        return (c >= 'A' && c <= 'H') || (c >= 'a' && c <= 'h');
+    }
+
+    /**
+     * Ensures the character used to represent the column is valid and not outside the bounds.
+     * @param c The character that represents the column.
+     * @return True if the character is a valid column index.
+     */
+    private static boolean isValidColIndex(char c) {
+        return c >= '1' && c <= '8';
+    }
+
+    /**
+     * Used to check whether a move is the default move, aka initialised with the default constructor.
+     * @return True if it is the default move, false otherwise.
+     */
+    public boolean isDefault() {
+        return this.row == -1 && this.col == -1;
     }
 }
